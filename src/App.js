b679ee00459c4af3d873d7c334ca13fc6cf8c2f3
Karan V <karanvknarayanan@gmail.com>
2020-05-14 13:04:03 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 function App() {
   return (
     <div className="App">
-      <h1> Hi this is karan</h1>
+      <h1 > Hi this is karan</h1>
+      <Person name="James" age="19"> hello</Person>
+      
+      
+      
     </div>
   );
+// return React.createElement('div',{className:'App'},React.createElement('h1',null,'hi I\' karan!!'))
+
 }
+
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react'
+const person = (props) => {
+    return(
+ <div> 
+
+ <p>my name is {props.name} I am  {props.age} old </p>
+      
+ </div>
+       );
+        
+    
+
+}
+export default person
